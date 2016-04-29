03662e7df8bbc5dfeca1ee793d9987879848da3d
Jill Ye <jiyou.linux@gmail.com>
2016-04-29 14:18:28 +0800

    update search

--- js/search.js
+++ js/search.js
@@ -1,58 +1,43 @@
-jQuery(function() {
-  // Initalize lunr with the fields it will be searching on. I've given title
-  // a boost of 10 to indicate matches on this field are more important.
+var params = {};
+  if (location.search) {
+    var parts = location.search.substring(1).split('&');
+    for (var i = 0; i < parts.length; i++) {
+        var nv = parts[i].split('=');
+        if (!nv[0]) continue;
+        params[nv[0]] = nv[1] || true;
+    }
+  }
+
   window.idx = lunr(function () {
-    this.field('id');
+    this.use(lunr.jp);
+    this.field('post_id');
     this.field('title', { boost: 10 });
-    this.field('author');
     this.field('category');
     this.field('tags');
   });
 
-  // Download the data from the JSON file we generated
   window.data = $.getJSON('/search_data.json');
-
-  // Wait for the data to load and add it to lunr
   window.data.then(function(loaded_data){
     $.each(loaded_data, function(index, value){
       window.idx.add(
         $.extend({ "id": index }, value)
       );
     });
+    var results = window.idx.search(decodeURIComponent(params['string']));
+    results.forEach(function(result) {
+      var item = loaded_data[result.ref];
+      var title = $("<td></td>").append($("<a></a>").text(item.title).attr("href", item.url));
+      var date = $("<td></td>").addClass("page_datetime").text(item.date);
+      var cat = $("<td></td>").append(
+        $("<span></span>").append(
+          $("<a></a>").attr("class", "label label-default").attr("href", "/type#" + item.category).text(item.category)));
+      var comments = $("<td></td").append($("<span></span>").attr("class", "badge ds-thread-count").attr("data-thread-key", item.post_id).attr("data-count-type","comments"));
+      $("#result").append(
+        $("<tr></tr")
+          .append(title)
+          .append(date)
+          .append(cat)
+          .append(comments)
+          );
+      });
   });
-
-  // Event when the form is submitted
-  $("#site_search").submit(function(event){
-      event.preventDefault();
-      var query = $("#search_box").val(); // Get the value for the text field
-      var results = window.idx.search(query); // Get lunr to perform a search
-      console.log(results);
-      display_search_results(results); // Hand the results off to be displayed
-  });
-
-  function display_search_results(results) {
-    var $search_results = $("#search_results");
-
-    // Wait for data to load
-    window.data.then(function(loaded_data) {
-
-      // Are there any results?
-      if (results.length) {
-        $search_results.empty(); // Clear any old results
-
-        // Iterate over the results
-        results.forEach(function(result) {
-          var item = loaded_data[result.ref];
-
-          // Build a snippet of HTML for this result
-          var appendString = '<li><a href="' + item.url + '">' + item.title + '</a></li>';
-
-          // Add it to the results
-          $search_results.append(appendString);
-        });
-      } else {
-        $search_results.html('<li>No results found</li>');
-      }
-    });
-  }
-});
