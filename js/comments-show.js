8678dfe05a4abc02e5d850cda923744d1204c644
JiYouMCC <nightghostjiyou@gmail.com>
2017-03-30 20:49:50 +0800

    update show comments

---
+++ js/comments-show.js
@@ -0,0 +1,87 @@
+var comments_messages = undefined;
+
+function showError(error) {
+  var message = error.message ? error.message : error;
+  $("#modal_error").modal('show');
+  $("#error_message").text(message);
+}
+
+Comments.handleError = function(error) {
+  showError(error);
+};
+
+Comments.comment.listCallback("{{ page.id }}", function(messages) {
+  comments_messages = messages
+  $('div[id^=comments_]').remove();
+  for (commentId in messages) {
+    var comment = messages[commentId];
+    var comment_detail = $("<div></div>");
+    if (comment.reply) {
+      comment_detail.append(
+        $("<blockquote></blockquote>").append(
+          $("<div></div>").append(
+            $("<img></img>").addClass("img-circle").attr("alt", messages[comment.reply].name).attr('src', getGravatar(messages[comment.reply].email, 20))
+          ).append(" ").append(
+            $("<strong></strong>").text(messages[comment.reply].name)
+          ).append(" ").append(
+            $("<span></span>").addClass('page_datetime').text(Comments.formatDate(new Date(parseInt(messages[comment.reply].timestamp))))
+          ).append(
+            $("<pre></pre>").text(messages[comment.reply].comment)
+          )
+        )
+      )
+    }
+    comment_detail.append(
+      $("<pre></pre>").text(comment.comment)
+    );
+    var name_div = $("<div></div>").append(
+      $("<img></img>").addClass("img-circle").attr("alt", comment.name).attr('src', getGravatar(comment.email, 20))
+    ).append(" ").append(
+      $("<strong></strong>").text(comment.name)
+    ).append(" ").append(
+      $("<span></span>").addClass('page_datetime').text(Comments.formatDate(new Date(parseInt(comment.timestamp))))
+    );
+
+    if (comment.url) {
+      name_div.append(" ").append(
+        $("<a></a>").attr('href', comment.url).attr('target','_blank').attr('title', "网站").append(
+          $("<button></button>").addClass('btn btn-link').append(
+            $("<span></span>").addClass("glyphicon glyphicon-home")
+          )
+        )
+      );
+    }
+
+    name_div.append(" ").append(
+      $("<button></button>").addClass('btn btn-link').attr('title', "回复").attr('id', "button_reply_" + commentId).attr('reply', commentId).append(
+        $("<span></span>").addClass("glyphicon glyphicon-comment")
+      )
+    );
+
+    var d_comments = $("<div></div>").addClass("list-group-item").attr('id', "comments_" + commentId).append(name_div).append(comment_detail);
+    d_comments.insertAfter($("#div_comments"));
+  }
+
+  $("button[id^=button_reply_]").click(function() {
+    var reply = $(this).attr("reply");
+    $("#reply").val(reply);
+    $("#reply_user").text(comments_messages[reply].name);
+    $("#reply_to").show();
+  });
+});
+
+$("#add_comment").click(function() {
+  $("#add_comment").attr("disabled", "");
+  Comments.comment.add($("#name").val(), $("#email").val(), '{{ page.id }}', $("#comment").val(), $("#url").val(), $("#reply").val(), function(result) {
+    if (result) {
+      $("#comment").val("");
+    }
+
+    $("#add_comment").removeAttr("disabled");
+  });
+});
+
+$("#reply_clear").click(function() {
+  $("#reply").val("");
+  $("#reply_to").hide();
+});
