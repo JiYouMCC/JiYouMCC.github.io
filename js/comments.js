bfe1820a882cf139a770ddc7d808d74f6c7c7085
Jill Ye <jill.ye@perkinelmer.com>
2017-03-30 12:47:58 +0800

    Update

--- js/comments.js
+++ js/comments.js
@@ -20,8 +20,8 @@ Comments = {
     },
     formatDate: function(date) {
         return date.getFullYear() + "-" + 
-            ("0"+(date.getMonth() + 1)).slice(-2) + 
-            ("0"+(date.getDate() + 1)).slice(-2) + 
+            ("0"+(date.getMonth() + 1)).slice(-2) + "-" + 
+            ("0"+date.getDate()).slice(-2) + " " +
             ("0" + date.getHours()).slice(-2) + ":" + 
             ("0" + date.getMinutes()).slice(-2) + ":" + 
             ("0" + date.getSeconds()).slice(-2) + " ";
