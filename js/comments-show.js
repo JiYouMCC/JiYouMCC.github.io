a012dd80f9d1c31178e60f60e020e414eb545a08
Jill Ye <jill.ye@perkinelmer.com>
2017-04-01 10:46:00 +0800

    update alert

--- js/comments-show.js
+++ js/comments-show.js
@@ -37,9 +37,13 @@ function showComments(page) {
         $("<div></div>").addClass('page_blog_comment_message').text(comment.comment)
       );
 
+      alert(comment.name);
+      alert(comment.email);
+      alert(comment.timestamp);
+      alert(comment.comment);
       var name_div = $("<div></div>").append(
-//        $("<img></img>").addClass("img-circle").attr("alt", comment.name).attr("src", getGravatar(comment.email, 20))
-//      ).append(" ").append(
+        $("<img></img>").addClass("img-circle").attr("alt", comment.name).attr("src", getGravatar(comment.email, 20))
+      ).append(" ").append(
         $("<strong></strong>").text(comment.name)
       ).append(" ").append(
         $("<span></span>").addClass('page_datetime').text(Comments.formatDate(new Date(parseInt(comment.timestamp))))
