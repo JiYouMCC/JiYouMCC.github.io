c555ebb8857f886914d05cf40a2e72b7f4dae9a7
Ji You <nightghostjiyou@gmail.com>
2017-04-06 10:41:56 +0800

    Update comments-recent.js

--- js/comments-recent.js
+++ js/comments-recent.js
@@ -4,15 +4,13 @@ Comments.comment.recent.updateCallback(7, function(comments) {
     var comment = comments[i];
     var commentId = comment.id;
     $("#recent-comments").prepend(
-      $("<a></a>").addClass("list-group-item").attr("href", comment.post + "#comments_" + commentId).append(
+      $("<a></a>").attr("data-toggle", "tooltip").attr("data-placement", "bottom").attr("title", Comments.formatDate(new Date(parseInt(comment.timestamp)))),addClass("list-group-item").attr("href", comment.post + "#comments_" + commentId).append(
         $("<div></div>").append(
           $("<span></span>").addClass("page_blog_comment_name").text(comment.name)
-        ).append(" ").append(
-          $("<span></span>").addClass('page_datetime').text(Comments.formatDate(new Date(parseInt(comment.timestamp))))
         )
       ).append(
         $("<div></div>").text(comment.comment.slice(0, 32))
       )
     );
   }
-})
+});
