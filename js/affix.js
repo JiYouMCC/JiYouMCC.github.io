72725b7a52cbd6cdc4d920731830872c6151fcd2
Ji You <nightghostjiyou@gmail.com>
2017-04-11 15:41:42 +0800

    Update affix.js

--- js/affix.js
+++ js/affix.js
@@ -15,58 +15,61 @@ $(function() {
   }); 
 });
 
-//<a href="#title_0" data-localize="Gender List">Gender List</a>  
-  var headers = $("#blog_content").find("h1,h2,h3,h4,h5,h6");
-  var maxHead = 6;
-  for (var i = 0; i < headers.length; i++) {
-    var currentHead = parseInt(headers[i].tagName[1]);
-    if (currentHead < maxHead) {
-      maxHead = currentHead;
-    }
+
+var headers = $("#blog_content").find("h1,h2,h3,h4,h5,h6");
+var maxHead = 6;
+for (var i = 0; i < headers.length; i++) {
+  var currentHead = parseInt(headers[i].tagName[1]);
+  if (currentHead < maxHead) {
+    maxHead = currentHead;
   }
-  
-  var links = $("<ol></ol>").attr("id", "affix-nav-ul").attr("class", "nav nav-stacked").attr("role", "tablist");
-  
-  links.append(
-    $("<li></li").append(
-      $("<a></a>").attr("href", "#top").append(
-        $("<span></span>").attr("class", "glyphicon glyphicon-triangle-top")
-      )
+}
+
+var links = $("<ol></ol>").attr("id", "affix-nav-ul").attr("class", "nav nav-stacked").attr("role", "tablist");
+
+links.append(
+  $("<li></li").append(
+    $("<a></a>").attr("href", "#top").append(
+      $("<span></span>").attr("class", "glyphicon glyphicon-triangle-top")
     )
-  );
-  
-  var currentParent = links;
-  var lastLi = null;
-  var currentClass = maxHead;
-  
-  for (var i = 0; i < headers.length; i++) {
-    var currentHead = parseInt(headers[i].tagName[1]);
-    if (currentHead > currentClass) {
-      while (currentHead > currentClass) {
-        var newUl = $("<ol></ol>");
-        if (lastLi != null){
-          lastLi.append(newUl)
-        } else {
-          currentParent.append(newUl);
-        }
-        currentClass += 1;
-        currentParent = newUl;
-      }
-    } else if (currentHead < currentClass) {
-      while (currentHead < currentClass) {
-        currentClass -= 1;
-        currentParent = currentParent.parent().parent();
-      }
+  )
+);
+
+var currentParent = links;
+var lastLi = null;
+var currentClass = maxHead;
+
+for (var i = 0; i < headers.length; i++) {
+  var currentHead = parseInt(headers[i].tagName[1]);
+  if (currentHead > currentClass) {
+    while (currentHead > currentClass) {
+      var newUl = $("<ol></ol>").addClass("nav").addClass("nav-stacked");
+      if (lastLi != null){
+        lastLi.append(newUl)
+      } else {
+        currentParent.append(newUl);
+      }  
+      
+      currentClass += 1;
+      currentParent = newUl;
     }
-    
-    lastLi = $("<li></li").append($("<a></a>").attr("href", "#" + headers[i].id).text($(headers[i]).text()));
-    currentParent.append(lastLi);
-  }
-  
-  links.append(
-    $("<li></li").append(
-      $("<a></a>").attr("href", "#bottom").append(
-        $("<span></span>").attr("class", "glyphicon glyphicon-triangle-bottom")
-      )
-    )
+  } else if (currentHead < currentClass) {
+    while (currentHead < currentClass) {
+      currentClass -= 1;
+      currentParent = currentParent.parent().parent();
+    }
+  } 
+  //<a href="#title_0" data-localize="Gender List">Gender List</a>
+  lastLi = $("<li></li>").append(
+    $("<a></a>").attr("href", "#" + headers[i].id).attr("data-localize",$(headers[i]).text()).text($(headers[i]).text())
   );
+  currentParent.append(lastLi);
+}
+
+links.append(
+  $("<li></li").append(
+    $("<a></a>").attr("href", "#bottom").append(
+      $("<span></span>").attr("class", "glyphicon glyphicon-triangle-bottom")
+    )
+  )
+);
