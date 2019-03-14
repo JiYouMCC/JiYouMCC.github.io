c52b1f3654d44a33c4d29fa97e6d8d39fda07d2a
Ji You <nightghostjiyou@gmail.com>
2019-03-14 17:06:49 +0800

    Update git-comments-count.js

--- js/git-comments-count.js
+++ js/git-comments-count.js
@@ -3,10 +3,11 @@ $(function() {
     for (var i = 0; i < elements.length; i++) {
         var element = $(elements[i]);
         var issueId = element.attr('comments-count');
+        var postId = element.attr('post-id');
         if (issueId) {
             GithubComments.Comments.Count(issueId, function(result) {
                 if (result.status) {
-                    $("[comments-count='" + result.number + "']").text(result.count);
+                    $("[comments-count='" + result.number + "']").text(result.count + OldComments.GetCommentsCount(postId));
                 }
             });
         }
