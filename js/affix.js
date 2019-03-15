4e25b65265341067ca2a40861f70192e0c48daf4
Ji You <nightghostjiyou@gmail.com>
2019-03-15 11:19:50 +0800

    Update affix.js

--- js/affix.js
+++ js/affix.js
@@ -71,7 +71,7 @@ for (var i = 0; i < headers.length; i++) {
 
 links.append(
   $("<li></li").append(
-    $("<a></a>").attr("affix_to","#comments").attr('href', '#comments').append(
+    $("<a></a>").attr("affix_to","#comments").attr('href', '#comment_panel').append(
       $("<span></span>").attr("class", "glyphicon glyphicon-comment")
     ).append(" 评论")
   )
