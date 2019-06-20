a35f4591f40780151df9dbea93d937a248809f82
Jill Ye <jill.ye@perkinelmer.com>
2019-06-20 13:46:36 +0800

    Update

--- js/ascii-art.js
+++ js/ascii-art.js
@@ -50,3 +50,42 @@ function getColorChars(colorStr) {
   });
   return colorArray.join("");
 }
+
+// UI update
+
+function setZoom() {
+  var range = document.getElementById("range");
+  var range_value = range.value;
+  document.getElementById("ascii").style.transform = "scale(" + (range_value).toString() + ")";
+}
+
+function updateCanvas() {
+  var imgInput = document.getElementById("img_input");
+  if (imgInput.files && imgInput.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var img = document.getElementById("img");
+      img.src = e.target.result;
+    }
+    reader.readAsDataURL(imgInput.files[0]);
+  }
+}
+
+function getASCIICode(ctx, width, height, colorStr) {
+  var ascii = "";
+  var greyImg = ctx.createImageData(width, height);
+  var greyData = greyImg.data;
+  var sourceImg = ctx.getImageData(0, 0, width, height);
+  var sourceData = sourceImg.data;
+
+  for (i = 0; i < sourceData.length; i += 4) {
+    luma = Math.floor((sourceData[i] + sourceData[i + 1] + sourceData[i + 2]) / 3);
+    greyData[i] = greyData[i + 1] = greyData[i + 2] = luma;
+    ascii += colorStr[Math.floor(luma * colorStr.length / 256)];
+    greyData[i + 3] = sourceData[i + 3];
+    if ((i / 4 + 1) % width == 0) {
+      ascii += "\n";
+    }
+  }
+  return ascii;
+}
