0b20d3e99f59687d5787a2a880c639206dd01a42
Ji You <7357434+JiYouMCC@users.noreply.github.com>
2021-02-22 10:51:07 +0800

    Update git-comments-show.js

--- js/git-comments-show.js
+++ js/git-comments-show.js
@@ -93,7 +93,7 @@ function addComment(comment) {
   );
 
   var d_comments = $("<div></div>").addClass("list-group-item").attr('id', "comments_" + commentId).append(name_div).append(comment_detail);
-  $("#git_comments").prepend(d_comments);
+  $("#git_comments").append(d_comments);
   document.getElementById('comment_text_' + commentId).innerHTML = comment.body_html;
   emojiProcess();
 }
