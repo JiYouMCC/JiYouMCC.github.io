76857d7f24ccc47353c93c025a0c9fa13e99fd3e
JiYouMCC <chengyi.ye@outlook.com>
2024-09-10 15:55:51 +0800

    update

--- js/search.js
+++ js/search.js
@@ -17,29 +17,50 @@ async function search(searchString) {
   pagefind.init();
   var search = await pagefind.search(searchString);
   var results = await Promise.all(search.results.map(r => r.data()));
-
   $("#pagefind_result").text(" ");
   $("#search_message").html("搜索“" + searchString + "”，查找到 " + results.length + " 条记录");
   for (var i = 0; i < results.length; i++) {
-    var div_left = $("<div></div>").addClass("media-left");
+    var row = $("<div></div>").addClass("row g-0");
     if ("image" in results[i]["meta"]) {
-      div_left.append(
-        $("<img>").addClass("media-object").attr("src", results[i]["meta"]['image']).attr("title", results[i]["meta"]['title']).attr("alt", results[i]["meta"]['title']).css("max-width", "75px").css("max-height", "150px")
+      var image_div = $("<div></div>").addClass("col-md-3");
+      image_div.append(
+        $("<img>")
+          .attr("src", results[i]["meta"]['image'])
+          .attr("title", results[i]["meta"]['title'])
+          .attr("alt", results[i]["meta"]['title'])
+          .attr("style", "max-width: 100%")
       )
-    };
-    var div_body_title = $("<a></a>").attr('href', results[i]["url"]).attr('target', "_blank").append($("<h4></h4>").addClass("media-heading").text(results[i]["meta"]['title']));
-    var div_body_p = $("<p>" + results[i]['excerpt'] + "</p>");
-    var div_body = $("<div></div>").addClass("media-body");
-    div_body.append(div_body_title);
-    div_body.append(div_body_p);
+      row.append(image_div);
 
-    var div_media = $("<div></div>").addClass("media");
-    div_media.append(div_left);
-    div_media.append(div_body);
-
-    $("#pagefind_result").append(
-      $("<li></li>").addClass("list-group-item").append(div_media)
-    )
+      var text_div = $("<div></div>").addClass("col-md-9");
+      var div_body_title = $("<a></a>")
+        .attr('href', results[i]["url"])
+        .attr('target', "_blank")
+        .append($("<h4></h4>")
+          .text(results[i]["meta"]['title'])
+      );
+      var div_body_p = $("<p>" + results[i]['excerpt'] + "</p>");
+      var div_body = $("<div></div>").addClass("card-body");
+      div_body.append(div_body_title);
+      div_body.append(div_body_p);
+      text_div.append(div_body);
+      row.append(text_div);
+    } else {
+      var text_div = $("<div></div>").addClass("col-md-12");
+      var div_body_title = $("<a></a>")
+        .attr('href', results[i]["url"])
+        .attr('target', "_blank")
+        .append($("<h4></h4>")
+          .text(results[i]["meta"]['title'])
+      );
+      var div_body_p = $("<p>" + results[i]['excerpt'] + "</p>");
+      var div_body = $("<div></div>").addClass("card-body");
+      div_body.append(div_body_title);
+      div_body.append(div_body_p);
+      text_div.append(div_body);
+      row.append(text_div);
+    }
+    $("#pagefind_result").append(row);
   }
   //console.log(results)
 }
